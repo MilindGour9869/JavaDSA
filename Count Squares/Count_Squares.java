a855da8d5bf89301e8bfb37e699c801f9d0676f2
spidy9869 <63071549+spidy9869@users.noreply.github.com>
2022-03-29 15:33:06 +0530

    Count Squares

---
+++ Count Squares/Count_Squares.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+
+public class Count_Squares{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i;
+
+         for( i=1;i*i<n;i++)
+         {
+
+
+         }
+
+         System.out.println(i-1);
+
+
+
+    }
+}
